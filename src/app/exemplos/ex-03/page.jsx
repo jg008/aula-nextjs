13cb7eba2656043cb85834f00b42ea9eb32a7a28
jg008 <joaonc176@gmail.com>
2026-03-20 13:17:35 -0300

    13:17

--- src/app/exemplos/ex-03/page.jsx
+++ src/app/exemplos/ex-03/page.jsx
@@ -1,3 +1,21 @@
-'use cleient'
+'use client'
 
-import 
+import styles from "./page.module.css";
+
+import { useState } from "react";
+
+export default function Exemplo03() {  
+
+    const [num, setNum] = useState(100);
+
+    function handleIncrementa () {
+        setNum(num + 1);
+    }
+
+    return (
+        <>
+            <label>{`Contador: ${num}`}</label>            
+            <label onClick={() => handleIncrementa()}>+1</label>
+        </>
+    );
+}
