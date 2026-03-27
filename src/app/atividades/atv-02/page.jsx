58c9b8b715fccee2060b6162f81ce4e14242e719
jg008 <joaonc176@gmail.com>
2026-03-27 11:45:19 -0300

    aula

--- src/app/atividades/atv-02/page.jsx
+++ src/app/atividades/atv-02/page.jsx
@@ -1,41 +1,41 @@
-import Link from 'next/link';
+'use client'
 
-import styles from "./page.module.css";
-function Atividades02() {
-    return(
-        <div>
-            <h1>Atividade 2</h1>
-            <p>Segunda Atividade de React</p>
-            <p>Atividade feita em Aula, baseada nos conhecimentos aprendidos em Sala</p>
-            <h2>Curiosidades Sobre a Copa do Mundo</h2>
-            <h3>Fatos históricos e recordes:</h3>
-            <p>A primeira Copa do Mundo foi realizada em 1930 no Uruguai,
-             com apenas 13 seleções, e o país anfitrião sagrou-se campeão ao vencer a Argentina por 4 a 2. 
-             Desde então, o torneio ocorre a cada quatro anos, exceto em 1942 e 1946, devido à Segunda Guerra Mundial. 
-             O Brasil é o país com mais títulos, totalizando cinco conquistas, e é a única seleção a ter participado de todas as edições. 
-             Alemanha e Itália possuem quatro títulos cada, enquanto a Argentina tem três.
-             Pelé é o único jogador a vencer três Copas do Mundo (1958, 1962 e 1970),. 
-             O alemão Miroslav Klose é o maior artilheiro da história do torneio, com 16 gols, e o francês Just Fontaine detém o recorde de mais gols em uma única edição, com 13 em 1958. 
-             Lionel Messi e Cristiano Ronaldo são os únicos a marcar em quatro edições diferentes, enquanto Ronaldo Fenômeno é o segundo maior artilheiro, com 15 gols. 
-             Momentos inusitados Alguns episódios curiosos incluem: o jogador uruguaio Héctor Castro, apelidado de “o divino manco”, que venceu a Copa de 1930 sem uma das mãos; 
-             o primeiro gol da história da Copa, marcado pelo francês Lucien Laurent em 1930; 
-             e um cachorro que invadiu o campo durante Brasil x Inglaterra em 1962. 
-             A maior goleada da história ocorreu em 1982, quando a Hungria venceu El Salvador por 10 a 1.</p>
-       
-        <br></br>
+import { useState } from "react";
 
-        <label>Esvcreva um Comentário sobre as curiosidades acima</label>
+import styles from './page.module.css';
 
-        <br></br>
-        <br></br>
+export default function Atividade3() {  
 
-        <textarea></textarea>
+    const [num, setNum] = useState(100);
 
-        <button>Enviar</button>
+    function handleIncrementa () {
+        setNum(num + 1);
+    }
+    function handleredução () {
+        setNum(num - 1);
+    }
+
+    return (
         
+        <div className={styles.container}>
+
+            <h1>Atividade 2</h1>
+            <p>Contador incrementando e decrementando com uso de useState do React</p>
+
+            <label className={styles.texto}>{`Contador: ${num}`}</label>
+
+
+            <label 
+                onClick={() => handleIncrementa()}
+                className={styles.botao}
+            >+1</label>
+            
+
+            <label 
+                onClick={() => handleredução()}
+                className={styles.botao}
+            >-1</label>
+            
         </div>
-
-    )
+    );
 }
-
-export default Atividades02;
