23d84385d6b5bb49c8cdce0673f9a38b6a5af94b
jg008 <joaonc176@gmail.com>
2026-05-15 11:43:50 -0300

    15/05

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -13,6 +13,7 @@ function Home() {
         <Link href="/exemplos/ex-04">Exemplo 4</Link>
         <Link href="/exemplos/ex-05">Exemplo 5</Link>
         <Link href="/exemplos/ex-06">Exemplo 6</Link>
+        <Link href="/exemplos/ex-07">Exemplo 7</Link>
       </div>
       <div className={styles.containerListas}>
         <h1>Atividades</h1>
